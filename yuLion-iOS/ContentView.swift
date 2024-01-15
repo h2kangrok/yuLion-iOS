a4ff7aa7192a90f817ad55baeaf1b3a7b7c219bc
yanni <122153297+yanni13@users.noreply.github.com>
2024-01-15 20:47:47 +0900

    first commit

---
+++ yuLion-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yuLion-iOS
+//
+//  Created by 아우신얀 on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yuLion-iOS/yuLion_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuLion_iOSApp.swift
+//  yuLion-iOS
+//
+//  Created by 아우신얀 on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct yuLion_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
